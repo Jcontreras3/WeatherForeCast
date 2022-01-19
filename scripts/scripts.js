74338835ca56bda17ebb9c7881c6ed9daadb4c2c
Jovann Contreras <jcontreras@codestack.co>
2022-01-19 11:35:31 -0800

    updated js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,8 @@
 function Getweather(){
     fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=47c793edde19307a0101a22dead25af6")
-    .then((resp) => resp.json())
-    .then((data) => {
-        console.log(data);
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data.clouds);
     });
 }
-
 Getweather();
