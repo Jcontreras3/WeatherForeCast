70410bb2972412b8aebde0a9f7f3a28ce6a8d04f
Jovann Contreras <jcontreras@codestack.co>
2022-01-19 11:47:15 -0800

    Added image to background

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@ function Getweather(){
     fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=47c793edde19307a0101a22dead25af6")
     .then(resp => resp.json())
     .then(data => {
-        console.log(data.clouds);
+        console.log(data);
     });
 }
 Getweather();
