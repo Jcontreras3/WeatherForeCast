bdd13aee57f976b7c979978f76c6a0a4c9055915
Jovann Contreras <jcontreras@codestack.co>
2022-01-19 14:05:04 -0800

    Added input box and btn

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
-// let Input = document.getElementById('Input');
-// let testBtn = document.getElementById('testBtn');
+ let Input = document.getElementById('Input');
+ let testBtn = document.getElementById('testBtn');
 function Getweather(){
     fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=47c793edde19307a0101a22dead25af6")
     .then(resp => resp.json())
@@ -9,14 +9,14 @@ function Getweather(){
 }
 Getweather();
 
-// testBtn.addEventListener('click', function(){
-//     GetInput();
-// })
+ testBtn.addEventListener('click', function(){
+     GetInput();
+ })
 
-// function GetInput(){
-//     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${Input.value}&APPID=47c793edde19307a0101a22dead25af6`)
-//     .then(resp => resp.json())
-//     .then(data =>{
-//         Input = data;
-//     })
-// }
+ function GetInput(){
+     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${Input.value}&APPID=47c793edde19307a0101a22dead25af6`)
+     .then(resp => resp.json())
+     .then(data =>{
+         console.log(data);
+     })
+ }
