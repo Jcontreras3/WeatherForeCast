ed024cbdaf848d6c5923ceb38aade45d37d3b944
Jovann Contreras <jcontreras@codestack.co>
2022-01-20 15:54:01 -0800

    made some adjustments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,12 +2,13 @@
  let Input = document.getElementById('Input');
  let searchBtn = document.getElementById('searchBtn');
  let result = document.getElementById('result')
+ 
 function Getweather(){
     fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=47c793edde19307a0101a22dead25af6")
     .then(resp => resp.json())
     .then(data => {
         result.textContent= data.name;
-    });
+    })
 }
 Getweather();
 
@@ -24,12 +25,12 @@ Getweather();
      })
  }
 
- function GetweatherType(){
-    fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=47c793edde19307a0101a22dead25af6")
-    .then(resp => resp.json())
-    .then(data => {
-        Weather.textContent= data.weather.main;
-        console.log(data.weather.main)
-    });
- }
- GetweatherType();
+  function GetweatherType(){
+     fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=47c793edde19307a0101a22dead25af6")
+     .then(resp => resp.json())
+     .then(data => {
+         Weather.textContent = data.weather[0].main.name;
+         console.log(data.weather[0].main.name)
+     });
+  }
+  GetweatherType();
