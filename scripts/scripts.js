f850f4411b014b65ae71e8c0dc8fb4172a846b2a
Jovann Contreras <jcontreras@codestack.co>
2022-01-21 14:20:48 -0800

    Added day 1 temp

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,6 +6,11 @@ import { dev, prod } from "./environment.js";
  let city = document.getElementById('city');
  let Temp = document.getElementById('Temp');
  let Day = document.getElementById('Day');
+ let Temp1 = document.getElementById('Temp1');
+ let Temp2 = document.getElementById('Temp2');
+ let Temp3 = document.getElementById('Temp3');
+ let Temp4 = document.getElementById('Temp4');
+ let Temp5 = document.getElementById('Temp5');
 
  let apiKey = '&units=imperial&APPID=';
 
@@ -20,13 +25,14 @@ function Getweather(){
     .then(resp => resp.json())
     .then(data => {
         city.textContent = data.name; Weather.textContent = data.weather[0].description; Temp.textContent = data.main.temp;
-        console.log(data.weather[0])
+        //console.log(data.weather[0])
     });
 }
 Getweather();
 
  searchBtn.addEventListener('click', function(){
      GetInput();
+     GetDays();
  })
 
  function GetInput(){
@@ -34,10 +40,28 @@ Getweather();
      .then(resp => resp.json())
      .then(data =>{
          city.textContent = data.name, Weather.textContent = data.weather[0].description, Temp.textContent = data.main.temp;
-         console.log(data);
-     })
+         //console.log(data);
+     });
  }
 
+ function GetDays1(){
+    fetch (`http://api.openweathermap.org/data/2.5/forecast?q=London,uk+${apiKey}`)
+    .then(resp => resp.json())
+    .then(data => {
+       Temp1.textContent = data.list[0].main.temp;
+       console.log(Temp1.textContent = data.list[0].main.temp)
+    });
+ }
+ GetDays1();
+
+ function GetDays(){
+     fetch (`http://api.openweathermap.org/data/2.5/forecast?q=${Input.value}+${apiKey}`)
+     .then(resp => resp.json())
+     .then(data => {
+        Temp1.textContent = data.list[0].main.temp;
+        console.log(Temp1.textContent = data.list[0].main.temp)
+     });
+ }
 //   function GetweatherType(){
 //      fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=47c793edde19307a0101a22dead25af6")
 //      .then(resp => resp.json())
