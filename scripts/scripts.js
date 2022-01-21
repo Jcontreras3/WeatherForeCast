ae4d851627a62369ba52d553746b424e9357c218
Jovann Contreras <jcontreras@codestack.co>
2022-01-21 15:40:09 -0800

    Got days to pop up in cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,12 +5,17 @@ import { dev, prod } from "./environment.js";
  let searchBtn = document.getElementById('searchBtn');
  let city = document.getElementById('city');
  let Temp = document.getElementById('Temp');
- let Day = document.getElementById('Day');
+ let CurrentDay = document.getElementById('CurrentDay');
  let Temp1 = document.getElementById('Temp1');
  let Temp2 = document.getElementById('Temp2');
  let Temp3 = document.getElementById('Temp3');
  let Temp4 = document.getElementById('Temp4');
  let Temp5 = document.getElementById('Temp5');
+ let Day1 = document.getElementById('Day1');
+ let Day2 = document.getElementById('Day2');
+ let Day3 = document.getElementById('Day3');
+ let Day4 = document.getElementById('Day4');
+ let Day5 = document.getElementById('Day5');
 
  let apiKey = '&units=imperial&APPID=';
 
@@ -19,16 +24,18 @@ import { dev, prod } from "./environment.js";
  }else{
      apiKey += dev.apiKey;
  }
- 
-function Getweather(){
+function GetDefaultLocation(){
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk+${apiKey}`)
     .then(resp => resp.json())
     .then(data => {
-        city.textContent = data.name; Weather.textContent = data.weather[0].description; Temp.textContent = data.main.temp;
+        city.textContent = data.name;
+         Weather.textContent = data.weather[0].description;
+         Temp.textContent = data.main.temp + "°F";
+         //CurrentDay.textContent = WeekDay(data.list[0].dt_txt);
         //console.log(data.weather[0])
     });
 }
-Getweather();
+GetDefaultLocation();
 
  searchBtn.addEventListener('click', function(){
      GetInput();
@@ -39,17 +46,33 @@ Getweather();
      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${Input.value}+${apiKey}`)
      .then(resp => resp.json())
      .then(data =>{
-         city.textContent = data.name, Weather.textContent = data.weather[0].description, Temp.textContent = data.main.temp;
+         city.textContent = data.name; Weather.textContent = data.weather[0].description;Temp.textContent = data.main.temp +"°F";
          //console.log(data);
      });
  }
+
+ function WeekDay(time){
+     let d = new Date(time);
+     let weekday = ["Saturday", "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"]
+     return weekday[d.getUTCDay()];
+ }
+ //WeekDay();
 
  function GetDays1(){
     fetch (`http://api.openweathermap.org/data/2.5/forecast?q=London,uk+${apiKey}`)
     .then(resp => resp.json())
     .then(data => {
-       Temp1.textContent = data.list[0].main.temp;
-       console.log(Temp1.textContent = data.list[0].main.temp)
+       Temp1.textContent = data.list[0].main.temp+"°F";
+       Day1.textContent = WeekDay(data.list[0].dt_txt);
+        Temp2.textContent = data.list[2].main.temp+"°F";
+        Day2.textContent = WeekDay(data.list[7].dt_txt); 
+        Temp3.textContent = data.list[3].main.temp+"°F";
+        Day3.textContent = WeekDay(data.list[14].dt_txt);
+         Temp4.textContent = data.list[4].main.temp+"°F";
+         Day4.textContent = WeekDay(data.list[28].dt_txt);
+          Temp5.textContent = data.list[5].main.temp+"°F";
+          Day5.textContent = WeekDay(data.list[35].dt_txt);
+       //console.log(Temp1.textContent = data.list[0].main.temp+"°F")
     });
  }
  GetDays1();
@@ -58,16 +81,16 @@ Getweather();
      fetch (`http://api.openweathermap.org/data/2.5/forecast?q=${Input.value}+${apiKey}`)
      .then(resp => resp.json())
      .then(data => {
-        Temp1.textContent = data.list[0].main.temp;
-        console.log(Temp1.textContent = data.list[0].main.temp)
+        Temp1.textContent = data.list[0].main.temp+"°F";
+        Day1.textContent = WeekDay(data.list[0].dt_txt);
+        Temp2.textContent = data.list[2].main.temp+"°F";
+        Day2.textContent = WeekDay(data.list[7].dt_txt); 
+        Temp3.textContent = data.list[3].main.temp+"°F";
+        Day3.textContent = WeekDay(data.list[14].dt_txt);
+         Temp4.textContent = data.list[4].main.temp+"°F";
+         Day4.textContent = WeekDay(data.list[28].dt_txt);
+          Temp5.textContent = data.list[5].main.temp+"°F";
+          Day5.textContent = WeekDay(data.list[35].dt_txt);
+        //console.log(Temp1.textContent = data.list[0].main.temp)
      });
  }
-//   function GetweatherType(){
-//      fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=47c793edde19307a0101a22dead25af6")
-//      .then(resp => resp.json())
-//      .then(data => {
-//          Weather.textContent = data.weather[0].main.name;
-//          console.log(data.weather[0].main.name)
-//      });
-//   }
-//   GetweatherType();
