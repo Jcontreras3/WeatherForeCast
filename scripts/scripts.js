48ee2084a3fc98233b6a88dbe9150914f1ed0f70
Jovann Contreras <jcontreras@codestack.co>
2022-01-21 09:40:01 -0800

    Added git Ignore

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,26 @@
+import { dev, prod } from "./environment.js";
+
  let Weather = document.getElementById('Weather');
  let Input = document.getElementById('Input');
  let searchBtn = document.getElementById('searchBtn');
  let city = document.getElementById('city')
+ let Temp = document.getElementById('Temp')
+
+ let apiKey = '&units=imperial&APPID=';
+
+ if(prod.isLive){
+     apiKey += prod.apiKey;
+ }else{
+     apiKey += dev.apiKey;
+ }
  
 function Getweather(){
-    fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=47c793edde19307a0101a22dead25af6")
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk+${apiKey}`)
     .then(resp => resp.json())
     .then(data => {
-        city.textContent= data.name, Weather.textContent = data.weather[0].description;
+        city.textContent = data.name; Weather.textContent = data.weather[0].description; Temp.textContent = data.main.temp;
         console.log(data.weather[0])
-    })
+    });
 }
 Getweather();
 
@@ -18,10 +29,10 @@ Getweather();
  })
 
  function GetInput(){
-     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${Input.value}&APPID=47c793edde19307a0101a22dead25af6`)
+     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${Input.value}+${apiKey}`)
      .then(resp => resp.json())
      .then(data =>{
-         city.textContent = data.name, Weather.textContent = data.weather[0].description;
+         city.textContent = data.name, Weather.textContent = data.weather[0].description, Temp.textContent = data.main.temp;
          console.log(data);
      })
  }
